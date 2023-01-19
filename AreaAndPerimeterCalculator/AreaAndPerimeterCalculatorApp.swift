a63ddc8fc88fb8c10f36d048de096ce51e2da053
Leon Gell <lgell@lcs.on.ca>
2023-01-19 11:05:07 -0500

    Initial Commit

---
+++ AreaAndPerimeterCalculator/AreaAndPerimeterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaAndPerimeterCalculatorApp.swift
+//  AreaAndPerimeterCalculator
+//
+//  Created by Leon Gell on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct AreaAndPerimeterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
